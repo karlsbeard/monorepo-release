e1569f0f315437f09f4e4ec1185899e2cf6cec88
karl <2557075675@qq.com>
2024-03-25 11:31:15 +0800

    feat: rebuild

--- packages/pkg-2/index.js
+++ packages/pkg-2/index.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // packages/pkg-b/index.ts
-var pkg_1_1 = require("pkg-1");
+var pkg_1_1 = require("@univer-ecology/pkg-1");
 (0, pkg_1_1.sum)(1, 2);
